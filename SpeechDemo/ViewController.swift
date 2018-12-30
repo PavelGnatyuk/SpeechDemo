bf49cd84774693184e162746903de2278e3b0fbd
PavelGnatyuk <pgnatyuk@gmail.com>
2018-12-30 17:37:51 +0200

    the logic implemented. but the app crashes when touch Talk button for the second time

--- SpeechDemo/ViewController.swift
+++ SpeechDemo/ViewController.swift
@@ -15,9 +15,28 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     @IBOutlet weak var buttonTalk: UIButton!
     @IBOutlet weak var buttonClear: UIButton!
     
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        speechRecognizer?.delegate = self
+        request.shouldReportPartialResults = true
+                
+        switch SFSpeechRecognizer.authorizationStatus() {
+        case .notDetermined:
+            askSpeechPermission()
+            
+        case .authorized:
+            self.buttonTalk.isEnabled = true
+            
+        case .denied, .restricted:
+            self.buttonTalk.isEnabled = false
+        }
+        
     }
 
     @IBAction func buttonClearClicked(_ sender: Any) {
@@ -25,11 +44,74 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     }
     
     @IBAction func buttonTalkClicked(_ sender: Any) {
-        textView.text += " Text for a test "
+        //textView.text += " Text for a test "
+        recognize()
+    }
+    
+    @IBAction func buttonStopClicked(_ sender: Any) {
+        stopListening()
+    }
+    
+    func askSpeechPermission() {
+        SFSpeechRecognizer.requestAuthorization { status in
+            OperationQueue.main.addOperation {
+                switch status {
+                case .authorized:
+                    self.buttonTalk.isEnabled = true
+                default:
+                    self.buttonTalk.isEnabled = false
+                }
+            }
+        }
     }
     
     func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
         debugPrint("Available: \(available)")
+        
+        if !available {
+            let alert = UIAlertController(title: "There was a problem accessing the recognizer", message: "Please try again later", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (action) in
+                self.dismiss(animated: true, completion: nil)
+            }))
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func recognize() {
+        let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, time) in
+            self.request.append(buffer)
+        }
+
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        }
+        catch {
+            debugPrint("Error: \(error)")
+        }
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { (result, error) in
+            var isFinal = false
+            
+            if result != nil {
+                self.textView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.stopListening()
+            }
+        })
+    }
+    
+    func stopListening() {
+        debugPrint("\(#function)")
+        audioEngine.stop()
+        audioEngine.inputNode.removeTap(onBus: 0)
+        recognitionTask = nil
     }
 }
 
