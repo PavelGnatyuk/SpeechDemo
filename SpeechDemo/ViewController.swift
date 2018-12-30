27af4efd4744d45f9001fd85ff79b3c827a54b76
PavelGnatyuk <pgnatyuk@gmail.com>
2018-12-30 16:26:51 +0200

    Initial Commit

---
+++ SpeechDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechDemo
+//
+//  Created by Pavel Gnatyuk on 30/12/2018.
+//  Copyright © 2018 Viber Media S.A.R.L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
